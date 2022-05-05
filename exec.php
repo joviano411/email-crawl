73bcaa30d4fd3da5da123f4684490aa925ec5f76
Chigozie Peter <ujahchigozie@gmail.com>
2022-05-05 23:43:43 +0800

    Update exec.php

--- exec.php
+++ exec.php
@@ -1,1 +1,11 @@
 
+<?php 
+define("PHP_SHELL_EXECUTION_PATH", "path/to/php");
+$crawOptions = array(
+    'target' => 'https://example.com',
+    'max' => 50,
+);
+$crawRequest = base64_encode(serialize($crawOptions));
+$crawScript =  __DIR__ . "/craw.php";
+$crawLogs =  __DIR__ . "/craw_logs.log";
+shell_exec(PHP_SHELL_EXECUTION_PATH . " " . $crawScript . " " . $crawRequest ." 'alert' >> " . $crawLogs . " 2>&1");
